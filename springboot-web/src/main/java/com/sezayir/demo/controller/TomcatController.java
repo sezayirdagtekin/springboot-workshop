86a0f230264fe5e3ea007a19e183054309012088
sezayirdagtekin <sezayir@hotmail.com>
2020-07-23 12:07:06 +0300

    ad  profile

--- springboot-web/src/main/java/com/sezayir/demo/controller/TomcatController.java
+++ springboot-web/src/main/java/com/sezayir/demo/controller/TomcatController.java
@@ -4,16 +4,20 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile("dev")
 @RestController
 public class TomcatController {
  
+  
     @GetMapping("/hello")
     public Collection<String> sayHello() {
         return IntStream.range(0, 10)
           .mapToObj(i -> "Hello number: " + i)
           .collect(Collectors.toList());
     }
+    
 }
