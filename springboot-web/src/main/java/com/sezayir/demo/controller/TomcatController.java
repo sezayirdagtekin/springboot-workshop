363db05926efbf68b72c81e38a1b91cd3a78d5af
sezayirdagtekin <sezayir@hotmail.com>
2020-07-27 12:55:33 +0300

    push commands

--- springboot-web/src/main/java/com/sezayir/demo/controller/TomcatController.java
+++ springboot-web/src/main/java/com/sezayir/demo/controller/TomcatController.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Profile("dev")
+//@Profile("dev")
 @RestController
 public class TomcatController {
  
